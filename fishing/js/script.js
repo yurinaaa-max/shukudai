94010b36cc4d10a7c97121c5b649e323e02b4b7c
yurinaaa-max <124154240+yurinaaa-max@users.noreply.github.com>
2024-09-05 21:50:01 +0900

    釣り餌サイトのコードです

---
+++ fishing/js/script.js
@@ -0,0 +1,25 @@
+    // topへ戻るボタン実装
+    // IE、Safari対応
+    // smoothscroll.js読み込み
+    // https://github.com/iamdustan/smoothscroll
+
+    // セレクタ名（.pagetop）に一致する要素を取得
+    const pagetop_btn = document.querySelector(".pagetop");
+
+    // .pagetopをクリックしたら
+    pagetop_btn.addEventListener("click", scroll_top);
+
+    // ページ上部へスムーズに移動
+    function scroll_top() {
+      window.scroll({ top: 0, behavior: "smooth" });
+    }
+
+    // スクロールされたら表示
+    window.addEventListener("scroll", scroll_event);
+    function scroll_event() {
+      if (window.pageYOffset > 100) {
+        pagetop_btn.style.opacity = "1";
+      } else if (window.pageYOffset < 100) {
+        pagetop_btn.style.opacity = "0";
+      }
+    }
